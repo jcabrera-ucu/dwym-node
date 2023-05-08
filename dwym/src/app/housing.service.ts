be21dc61d32a33c9ef7121d8984e2c930eddaecf
Juan Cabrera <j.cabrera@correo.ucu.edu.uy>
2023-05-08 20:59:02 -0300

    Wip

--- dwym/src/app/housing.service.ts
+++ dwym/src/app/housing.service.ts
@@ -13,6 +13,10 @@ export class HousingService {
     return this.housingLocationList.find(housingLocation => housingLocation.id === id);
   }
 
+  submitApplication(firstName: string, lastName: string, email: string) {
+    console.log(`Homes application recieved: firstName: ${firstName}, lastName: ${lastName}, email: ${email}.`);
+  }
+
   constructor() { }
 
   housingLocationList: HousingLocation[] = [
